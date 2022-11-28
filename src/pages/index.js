a915b89d037c5aa6f051a9f6eb81ee10950623bd
hue <32443158+huedev@users.noreply.github.com>
2022-11-27 19:22:05 -0600

    Turnip logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {
   ItchIcon,
   GitHubIcon,
 } from '../components/SocialIcons'
+import logoHuedev from '../images/huedev.png'
 import logoKnifemare from '../images/knifemare.png'
 import logoTextbox from '../images/textbox.png'
 
@@ -42,7 +43,7 @@ function ShowcaseProject({project}) {
   return (
     <li className="group relative flex flex-col items-stretch">
       <Link href={project.link.href} className="p-4 h-full rounded-2xl bg-zinc-100 transition hover:bg-zinc-200 dark:bg-zinc-800/50 hover:dark:bg-zinc-700/50">
-        <div className="relative flex h-8 w-8 items-center justify-center">
+        <div className="relative flex h-8 w-8 items-center justify-center shrink-0">
           <Image
             src={project.logo}
             alt=""
@@ -68,7 +69,7 @@ function Project({project}) {
     <li className="group relative flex flex-col items-stretch">
       <Link href={project.link.href} className="p-4 flex flex-row items-center gap-x-4 justify-between h-full rounded-2xl bg-zinc-100 transition hover:bg-zinc-200 dark:bg-zinc-800/50 hover:dark:bg-zinc-700/50" key={project.name}>
         <div className="flex flex-row items-center">
-          <div className="relative flex h-8 w-8 items-center justify-center flex-shrink-0">
+          <div className="relative flex h-8 w-8 items-center justify-center shrink-0">
             <Image
               src={project.logo}
               alt=""
@@ -113,28 +114,40 @@ export default function Home() {
       <main className="mx-auto max-w-7xl pt-12 px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
           <section>
-            <h1 className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
-              Hi, I&apos;m <span className="text-indigo-600 dark:text-indigo-500">huedev</span>!
-            </h1>
-            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-              I make games and art. I also enjoy tinkering with websites. I&apos;m currently working on a game called <strong>Knifemare</strong>.
-            </p>
-            <div className="mt-6 flex gap-6">
-              <SocialLink
-                href="https://mastodon.gamedev.place/@hue"
-                aria-label="Follow me on Mastodon"
-                icon={MastodonIcon}
-              />
-              <SocialLink
-                href="https://huedev.itch.io/"
-                aria-label="Follow me on itch.io"
-                icon={ItchIcon}
-              />
-              <SocialLink
-                href="https://github.com/huedev"
-                aria-label="Follow me on GitHub"
-                icon={GitHubIcon}
-              />
+            <div className="flex">
+              <div className="relative flex h-24 w-24 items-center justify-center shrink-0 sm:h-32 sm:w-32">
+                <Image
+                  src={logoHuedev}
+                  alt=""
+                  className="h-24 w-24 rounded-xl sm:h-32 sm:w-32"
+                  unoptimized
+                />
+              </div>
+              <div className="ml-8">
+                <h1 className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
+                  Hi, I&apos;m <span className="text-indigo-600 dark:text-indigo-500">huedev</span>!
+                </h1>
+                <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                  I make games and art. I also enjoy tinkering with websites. I&apos;m currently working on a game called <strong>Knifemare</strong>.
+                </p>
+                <div className="mt-6 flex gap-6">
+                  <SocialLink
+                    href="https://mastodon.gamedev.place/@hue"
+                    aria-label="Follow me on Mastodon"
+                    icon={MastodonIcon}
+                  />
+                  <SocialLink
+                    href="https://huedev.itch.io/"
+                    aria-label="Follow me on itch.io"
+                    icon={ItchIcon}
+                  />
+                  <SocialLink
+                    href="https://github.com/huedev"
+                    aria-label="Follow me on GitHub"
+                    icon={GitHubIcon}
+                  />
+                </div>
+              </div>
             </div>
           </section>
           
