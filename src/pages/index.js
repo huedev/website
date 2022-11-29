aa2bc389768d583056e09a2315fc9b161f87c248
hue <32443158+huedev@users.noreply.github.com>
2022-11-28 18:28:42 -0600

    External link icon for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const projects = [
     name: 'Knifemare',
     description:
       'A short and sweet action platformer about throwing and catching knives as a nightmare hunter. Currently in development with new info coming soon!',
-    link: 'https://huedev.itch.io/',
+    link: { href: 'https://huedev.itch.io/', external: true },
     logo: logoKnifemare,
     image: imageKnifemare,
   },
@@ -26,7 +26,7 @@ const projects = [
     name: 'Textbox',
     description:
       'A web app for manipulating text, lists, and other data.',
-    link: 'https://textbox.huedev.me/',
+    link: { href: 'https://textbox.huedev.me/', external: true },
     logo: logoTextbox,
     image: '',
   },
@@ -40,10 +40,19 @@ function SocialLink({ icon: Icon, ...props }) {
   )
 }
 
+function ExternalLinkIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" {...props}>
+      <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
+      <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
+    </svg>
+  )
+}
+
 function Project({project}) {
   return (
     <li className="group relative flex flex-col items-stretch">
-      <Link href={project.link} className="p-4 flex flex-col gap-x-4 h-full overflow-hidden rounded-2xl bg-zinc-100 transition hover:bg-zinc-200 dark:bg-zinc-800/50 hover:dark:bg-zinc-700/50" key={project.name}>
+      <Link href={project.link.href} rel="noopener noreferrer" target="_blank" className="p-4 flex flex-col gap-x-4 h-full overflow-hidden rounded-2xl bg-zinc-100 transition hover:bg-zinc-200 dark:bg-zinc-800/50 hover:dark:bg-zinc-700/50" key={project.name}>
         {
           project.image &&
           <div className="relative flex -mx-4 -mt-4 items-center justify-center shrink-0 mb-4">
@@ -66,9 +75,15 @@ function Project({project}) {
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h3 className="text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              {project.name}
-            </h3>
+            <div className="flex flex-row items-center gap-1">
+              <h3 className="text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                {project.name}
+              </h3>
+              {
+                project.link.external &&
+                <ExternalLinkIcon className="h-5 w-5 flex-none text-zinc-600 dark:text-zinc-400" />
+              }
+            </div>
             <p className="relative text-sm leading-relaxed text-zinc-600 dark:text-zinc-400">{project.description}</p>
           </div>
         </div>
@@ -130,7 +145,7 @@ export default function Home() {
             <h2 className="text-xl font-bold text-zinc-800 dark:text-zinc-100 sm:text-2xl">Projects</h2>
             <ul
               role="list"
-              className="grid grid-cols-1 gap-x-4 gap-y-4 mt-8"
+              className="grid grid-cols-1 gap-x-4 gap-y-4 mt-6"
             >
               {projects.map((project) => (
                 <Project
